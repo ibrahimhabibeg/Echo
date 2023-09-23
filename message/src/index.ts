14557d0ba5294baffedfb8b1605fcec8c1553217
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2023-09-23 10:36:55 +0300

    Added the message service

---
+++ message/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { connectDB } from "./db";
+
+const app = express();
+app.use(express.json());
+connectDB(app);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.on("dbConnected", () => {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+});
