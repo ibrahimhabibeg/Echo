946a27544519b5bd9afda9cb8d856c435d7113c7
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2023-09-23 15:27:17 +0300

    Added message model

---
+++ message/src/models/message.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    from: {
+      type: String,
+      required: true,
+    },
+    to: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("User", messageSchema);
+
+export default Message;
