aa5a93e7ac179a408d845f8407e032979688a00e
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2023-10-01 16:48:39 +0300

    Added useChatMessages

--- src/chat/Chat.tsx
+++ src/chat/Chat.tsx
@@ -1,7 +1,33 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { NavigationParamList } from "../Navigators/Logged";
+import useChatMessages from "./useChatMessages";
+import { FlatList } from "react-native";
 import { Text } from "react-native-paper";
+import { useEffect } from "react";
 
-const Chat = () => {
-  return <Text>Hello World!</Text>;
+const Chat = ({
+  route: {
+    params: { userId },
+  },
+}: props) => {
+  const { data, hasNextPage, isFetching, fetchNextPage, refetch } =
+    useChatMessages(userId);
+  useEffect(()=>{
+    console.log(data);
+    
+  },[data])
+  return (
+    <FlatList
+      data={data ? data.pages.flat(1) : []}
+      onEndReached={() => hasNextPage && !isFetching && fetchNextPage()}
+      refreshing={isFetching}
+      onRefresh={refetch}
+      keyExtractor={(message) => message._id}
+      renderItem={({ item }) => <Text>{item.message}</Text>}
+    />
+  );
 };
+
+type props = NativeStackScreenProps<NavigationParamList, "chat">;
 
 export default Chat;
---
+++ src/chat/useChatMessages.ts
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/Auth";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+const useChatMessages = (otherUser: string) => {
+  const { token } = useContext(AuthContext);
+  const query = useInfiniteQuery({
+    queryKey: ["chat", otherUser],
+    queryFn: async ({ pageParam }): Promise<messageType[]> => {      
+      const cursorString =
+        typeof pageParam === "undefined" ? "" : "cursor=" + pageParam;
+      console.log(`${process.env.EXPO_PUBLIC_MESSAGE_SERVICE_URL}/messages?otherUser=${otherUser}&${cursorString}`);
+      
+      const response = await fetch(
+        `${process.env.EXPO_PUBLIC_MESSAGE_SERVICE_URL}/messages?otherUser=${otherUser}&${cursorString}`,
+        { headers: { authorization: token } }
+      );      
+      if (!response.ok)
+        throw new Error("Can't reach server now. Please, try again later.");
+      const res = await response.json();      
+      return res.messages;
+    },
+    getNextPageParam: (lastPage) => {
+      if (lastPage.length === 0) return undefined;
+      else return lastPage[lastPage.length - 1].createdAt;
+    },
+  });
+  return query;
+};
+
+export default useChatMessages;
+
+interface messageType {
+  from: string;
+  to: string;
+  message: string;
+  createdAt: Date;
+  _id: string;
+}
